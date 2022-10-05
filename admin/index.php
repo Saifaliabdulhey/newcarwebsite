f7bd60d4f783879fe28d39449677d96446f83f28
On My Own <jspz887@gmail.com>
2022-10-06 02:15:35 +0300

    add url for sitemap

--- admin/index.php
+++ admin/index.php
@@ -1,10 +1,12 @@
 <?php require_once('header.php'); ?>
 
 <section class="content-header">
-	<h1>Dashboard</h1>
+  <h1>Dashboard</h1>
 </section>
 
 <?php
+
+
 $statement = $pdo->prepare("SELECT * FROM tbl_top_category");
 $statement->execute();
 $total_top_category = $statement->rowCount();
@@ -46,28 +48,56 @@ $statement->execute(array('Pending'));
 $total_order_pending = $statement->rowCount();
 
 $statement = $pdo->prepare("SELECT * FROM tbl_payment WHERE payment_status=? AND shipping_status=?");
-$statement->execute(array('Completed','Pending'));
+$statement->execute(array('Completed', 'Pending'));
 $total_order_complete_shipping_pending = $statement->rowCount();
 ?>
 
 <section class="content">
-<div class="row">
-            <div class="col-lg-3 col-xs-6">
-              <!-- small box -->
-              <div class="small-box bg-primary">
-                <div class="inner">
-                  <h3><?php echo $total_product; ?></h3>
+  <div class="row">
+    <div class="col-lg-3 col-xs-6">
+      <!-- small box -->
+      <div class="small-box bg-primary">
+        <div class="inner">
+          <h3><?php echo $total_product; ?></h3>
 
-                  <p>Number of Cars</p>
-                </div>
-                <div class="icon">
-                  <i class="ionicons ion-android-cart"></i>
-                </div>
-                
-              </div>
+          <p>Number of Cars</p>
+        </div>
+        <div class="icon">
+          <i class="ionicons ion-android-cart"></i>
+        </div>
 
-		  </div>
-		  
+      </div>
+
+    </div>
+
+    <form method="post" enctype="multipart/form-data">
+      <p>
+        <label for="firstName">Name:</label>
+        <input type="text" name="title" id="firstName" required>
+      </p>
+      <p>
+        <label for="lastName">VIN:</label>
+        <input type="text" name="slug" id="lastName" required>
+      </p>
+      <p>
+        <label for="emailAddress">Content:</label>
+        <input type="text" name="content" id="emailAddress" required>
+      </p>
+      <input type="submit" name="vin" value="Submit">
+    </form>
+    <?php
+
+
+    if (isset($_POST['vin'])) {
+      $title = $_POST['title'];
+      $slug = $_POST['slug'];
+      $content = $_POST['content'];
+
+      $statement = $pdo->prepare("INSERT INTO page (title, slug, content) VALUES('$title', '$slug', '$content')");
+      $statement->execute();
+    }
+
+    ?>
 </section>
 
 <?php require_once('footer.php'); ?>
